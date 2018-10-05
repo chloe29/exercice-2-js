c4959cb1a50219d3731767bb3ffe4fae706f6578
lavisse <chloe.lavisse80@gmail.com>
2018-10-05 16:34:57 +0200

    envoi des fichiers

---
+++ script.js
@@ -0,0 +1,5 @@
+var lastName = "Doe";
+var firstName = "John";
+var city = "New-York";
+// je concatène mes chaines de caractères avec les variables
+  alert("Nom : " + lastName + "\n" + "Prénom : " + firstName + "\n"+ "Ville : " + city);
